cf63774ffbb3178b4b20bb787f18e39228a8b220
Vathana <siengvathana1@gmail.com>
2026-04-23 23:49:52 +0700

    Fix FlowController to work with Spring WebFlux

--- src/main/java/com/example/API_Gatway/controller/FlowController.java
+++ src/main/java/com/example/API_Gatway/controller/FlowController.java
@@ -2,12 +2,18 @@ package com.example.API_Gatway.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class FlowController {
 
-    @GetMapping("/flow")
-    public String getFlow() {
-        return "forward:/flow.html";
+    @GetMapping(value = "/flow", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public Mono<Resource> getFlow() {
+        return Mono.just(new ClassPathResource("static/flow.html"));
     }
 }
