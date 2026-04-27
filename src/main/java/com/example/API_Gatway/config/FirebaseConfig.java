88902144a3422467cdc53b702069b30d93160968
Vathana <siengvathana1@gmail.com>
2026-04-27 18:20:10 +0700

    FIX: Update PostConstruct to jakarta for Spring Boot 3 compatibility

--- src/main/java/com/example/API_Gatway/config/FirebaseConfig.java
+++ src/main/java/com/example/API_Gatway/config/FirebaseConfig.java
@@ -4,8 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.IOException;
 
 @Configuration
